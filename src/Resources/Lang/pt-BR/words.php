067811ead83b64023e6a2db4bdd0caa5faa84dfc
Ricardo Rebello Sierra <contato@ricardosierra.com.br>
2019-11-07 08:05:14 -0300

    Novos arquivos

---
+++ src/Resources/Lang/pt-BR/words.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'dashboard' => 'Dashboard',
+    'controlPane' => 'Painel de Controle',
+    'articles' => 'Artigos',
+    'pages' => 'Páginas',
+    'photos' => 'Fotos',
+    'subscriptions' => 'Inscrições',
+    'addNew' => 'Adicionar novo',
+    'addNew' => 'Adicionar novo',
+    'edit' => 'Editar',
+    'name' => 'Nome',
+    'slug' => 'Slug',
+    'email' => 'Email',
+    'admin' => 'Nivel de Admin',
+    'action' => 'Ação',
+    'home' => 'Home',
+    'users' => 'Usuários',
+    'description' => 'Descrição',
+    'value' => 'Valor',
+    'moreInfo' => 'Mais informações',
+    'logOut' => 'Sair',
+    'features' => 'Features',
+    'back' => 'Voltar',
+    'dominio' => 'Dominio',
+    'client' => 'Cliente',
+    'id' => 'Identificador',
+    'updatedAt' => 'Atualizado em',
+    'createdAt' => 'Criado em',
+    'deletedAt' => 'Deletado em',
+    'euConcordo' => 'Eu concordo'
+];
