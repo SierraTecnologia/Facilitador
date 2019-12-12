a9e0c23f3881c19088bce1f441f2df2cc9532454
Ricardo Rebello Sierra <contato@ricardosierra.com.br>
2019-12-12 02:18:46 -0300

    Corrigindo Bugs

--- src/FacilitadorProvider.php
+++ src/FacilitadorProvider.php
@@ -552,10 +552,17 @@ class FacilitadorProvider extends ServiceProvider
     {
         $this->setDependencesAlias();
         (new Collection(self::$providers))->map(function ($provider) {
-            if (class_exists($provider)) {
-                $this->app->register($provider);
-            }
+            $this->app->register($provider);
         });
+
+        // Incluindo Debug
+        if ($this->app->environment('local', 'testing')) {
+            $this->app->register(DuskServiceProvider::class);
+        }
+        
+        if ($this->app->environment('local')) {
+            $this->app->register(DebugService::class);
+        }
     }
     private function setDependencesAlias()
     {
