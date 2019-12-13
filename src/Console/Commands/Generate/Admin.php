394c16fdab1691725cceeda27e4accf62fd2960e
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2019-12-13 07:17:52 -0300

    O criar admin cria tanto pro voyager qnt pro decoy

--- src/Console/Commands/Generate/Admin.php
+++ src/Console/Commands/Generate/Admin.php
@@ -3,6 +3,11 @@
 namespace Facilitador\Console\Commands\Generate;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Input\InputOption;
+use TCG\Voyager\Facades\Voyager;
 use Facilitador\Models\Admin as BkwldAdmin;
 
 class Admin extends Command
@@ -19,45 +24,135 @@ class Admin extends Command
      *
      * @var string
      */
-    protected $description = 'Create a new admin';
+    protected $description = 'Create a new admin. Make sure there is a user with the admin role that has all of the necessary permissions.';
+
+    /**
+     * Get user options.
+     */
+    protected function getOptions()
+    {
+        return [
+            ['create', null, InputOption::VALUE_NONE, 'Create an admin user', null],
+        ];
+    }
+    public function fire()
+    {
+        return $this->handle();
+    }
 
     /**
      * Create the new admin with input from the user
+     * Execute the console command.
      *
      * @return void
      */
     public function handle()
     {
-        $firstName = $this->ask('First Name');
-        $lastName = $this->ask('Last Name');
-        $email = $this->ask('Email');
-
-        // Check the email to see if its already being used
-        if ($admin = BkwldAdmin::where('email', $email)->exists()) {
-            $this->error('That email is already in use');
-
-            return;
+        // Get or create user
+        $user = $this->getUser(
+            $this->option('create')
+        );
+        // the user not returned
+        if (!$user) {
+            exit;
         }
+        // Get or create role
+        $role = $this->getAdministratorRole();
+        // Get all permissions
+        $permissions = Voyager::model('Permission')->all();
+        // Assign all permissions to the admin role
+        $role->permissions()->sync(
+            $permissions->pluck('id')->all()
+        );
+        // Ensure that the user is admin
+        $user->role_id = $role->id;
+        $user->save();
+        $this->info('The user now has full access to your site.');
+    }
 
-        $password = $this->secret('Enter your password');
-        $confirm = $this->secret('Confirm your password');
-
-        // password matching check
-        if ($password != $confirm) {
-            $this->error('Your passwords do not match.');
-
-            return;
+    /**
+     * Get command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['email', InputOption::VALUE_REQUIRED, 'The email of the user.', null],
+        ];
+    }
+    /**
+     * Get the administrator role, create it if it does not exists.
+     *
+     * @return mixed
+     */
+    protected function getAdministratorRole()
+    {
+        $role = Voyager::model('Role')->firstOrNew([
+            'name' => 'admin',
+        ]);
+        if (!$role->exists) {
+            $role->fill([
+                'display_name' => 'Administrator',
+            ])->save();
         }
+        return $role;
+    }
+    /**
+     * Get or create user.
+     *
+     * @param bool $create
+     *
+     * @return \App\User
+     */
+    protected function getUser($create = false)
+    {
+        $email = $this->argument('email');
+        $model = Auth::guard(app('VoyagerGuard'))->getProvider()->getModel();
+        $model = Str::start($model, '\\');
+        // If we need to create a new user go ahead and create it
+        if ($create) {
+            $name = $this->ask('Enter the admin name');
+            $password = $this->secret('Enter admin password');
+            $confirmPassword = $this->secret('Confirm Password');
+            // Ask for email if there wasnt set one
+            if (!$email) {
+                $email = $this->ask('Enter the admin email');
+            }
 
-        // Create a new admin
-        $admin = new BkwldAdmin;
-        $admin->first_name = $firstName;
-        $admin->last_name = $lastName;
-        $admin->email = $email;
-        $admin->password = $password;
+            // Check the email to see if its already being used
+            while ($admin = BkwldAdmin::where('email', $email)->exists()) {
+                $this->error('That email is already in use');
+                $email = $this->ask('Enter the admin email');
+            }
 
-        // Save out the new admin
-        $admin->save();
-        $this->info('Admin created!');
+            // Passwords don't match
+            while ($password != $confirmPassword) {
+                $this->info("Passwords don't match");
+                $password = $this->secret('Enter admin password');
+                $confirmPassword = $this->secret('Confirm Password');
+            }
+
+
+            // Create a new admin
+            $admin = new BkwldAdmin;
+            $admin->first_name = $name;
+            $admin->last_name = $name;
+            $admin->email = $email;
+            $admin->password = $password;
+            // Save out the new admin
+            $admin->save();
+
+            $this->info('Creating admin account');
+            return call_user_func($model.'::create', [
+                'name'     => $name,
+                'email'    => $email,
+                'password' => Hash::make($password),
+            ]);
+
+            
+        }
+        return call_user_func($model.'::where', 'email', $email)->firstOrFail();
+
     }
 }
