e918b16bc139993b3c8836c55cfac43beb17ffb6
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2019-12-13 13:52:14 -0300

    Corrigindo uses e namespaces

--- src/Console/Commands/FacilitadorInstallCommand.php
+++ src/Console/Commands/FacilitadorInstallCommand.php
@@ -133,7 +133,7 @@ class FacilitadorInstallCommand extends Command
         $this->call('storage:link');
 
         $this->info('Creating admin');
-        $this->call('facilitador:admin', ['--force' => '']);
+        $this->call('facilitador:admin', ['--create' => '']);
 
         $this->info('Successfully installed Facilitador! Enjoy');
     }
