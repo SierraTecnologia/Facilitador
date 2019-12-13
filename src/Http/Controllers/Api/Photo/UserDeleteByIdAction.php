e75c0abf9d56dce7490300a5d22be6eba8565e24
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2019-12-13 13:43:55 -0300

    Corrigindo uses e namespaces

--- src/Http/Controllers/Api/Photo/UserDeleteByIdAction.php
+++ src/Http/Controllers/Api/Photo/UserDeleteByIdAction.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Actions;
 
-use App\Models\Contracts\UserManager;
+use Informate\Managers\UserManager;
 use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
