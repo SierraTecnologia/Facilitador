8181dda8a6b3f7588a8fe0505ce65eeaa425211d
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-05-13 02:17:27 -0300

    Reorganizando arquivos do support

--- src/Services/FacilitadorService.php
+++ src/Services/FacilitadorService.php
@@ -6,7 +6,7 @@
 namespace Facilitador\Services;
 
 use Support\Components\Coders\Parser\ComposerParser;
-use Support\Helpers\Development\HasErrors;
+use Support\Traits\Debugger\HasErrors;
 
 
 use Symfony\Component\Debug\Exception\FatalThrowableError;
