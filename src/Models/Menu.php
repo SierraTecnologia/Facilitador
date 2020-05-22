0076922e6d20ffd7865b17b22fb003eb940c5044
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-05-22 16:25:30 -0300

    Ordem no Menu

--- src/Models/Menu.php
+++ src/Models/Menu.php
@@ -153,7 +153,7 @@ class Menu extends Model
         // Filter items by permission
         $items = $items->filter(
             function ($item) {
-                return !$item->children->isEmpty() || Auth::user()->can('browse', $item);
+                return !$item->children->isEmpty() || (Auth::user() && Auth::user()->can('browse', $item));
             }
         )->filter(
             function ($item) {
