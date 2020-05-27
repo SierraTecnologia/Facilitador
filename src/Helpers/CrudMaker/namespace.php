a0366091a4c648d7d33b02a1143325cd874c0ca7
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-05-27 08:24:28 -0300

    Rename

--- src/Helpers/CrudMaker/namespace.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!function_exists('app_namespace')) {
-    function app_namespace()
-    {
-        return app('Facilitador\Services\CrudMaker\AppService')
-            ->getAppNamespace();
-    }
-}
