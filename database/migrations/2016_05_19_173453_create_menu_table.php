5171494d87dcf1ce7eaeda83d6dc40eb1433dc6b
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-07-05 05:28:17 -0300

    Pagina de login abrindo corretamente #PPW-10

--- database/migrations/2016_05_19_173453_create_menu_table.php
+++ database/migrations/2016_05_19_173453_create_menu_table.php
@@ -16,6 +16,7 @@ class CreateMenuTable extends Migration
             'menus', function (Blueprint $table) {
                 $table->increments('id');
                 $table->string('name')->unique();
+                $table->string('slug')->default('main');
                 $table->timestamps();
             }
         );
