27c34ef13392671607d37f42e7bbd3def80de156
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-09-25 14:07:07 -0300

    Fix bugs

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -498,7 +498,7 @@
     $(".group_select_new").val('').trigger('change');
     </script>
     <iframe id="form_target" name="form_target" style="display:none"></iframe>
-    <form id="my_form" action="{{ route('profile.upload') }}" target="form_target" method="POST" enctype="multipart/form-data" style="width:0;height:0;overflow:hidden">
+    <form id="my_form" action="{{ route('profile.porteiro.upload') }}" target="form_target" method="POST" enctype="multipart/form-data" style="width:0;height:0;overflow:hidden">
         {{ csrf_field() }}
         <input name="image" id="upload_file" type="file" onchange="$('#my_form').submit();this.value='';">
         <input type="hidden" name="type_slug" id="type_slug" value="settings">
