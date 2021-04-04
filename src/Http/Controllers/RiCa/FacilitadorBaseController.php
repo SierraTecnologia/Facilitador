2d20eea9c895b67d3ba56bb29d09c8d39ce2b591
ricardo@sierratecnologia.com.br <ricardo@sierratecnologia.com.br>
2021-04-04 08:21:22 -0300

    RP-327 - Operador Funcionando melhor

--- src/Http/Controllers/RiCa/FacilitadorBaseController.php
+++ src/Http/Controllers/RiCa/FacilitadorBaseController.php
@@ -2,20 +2,9 @@
 
 namespace Facilitador\Http\Controllers\RiCa;
 
-use Exception;
-use Facilitador\Facades\Facilitador;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Facilitador\Http\Controllers\RiCa\Controller as BaseController;
+// use Facilitador\Http\Controllers\RiCa\Controller as BaseController;
+use Support\Http\Controllers\RiCa\Manager\FacilitadorBaseController as BaseController;
 // use Pedreiro\Http\Controllers\Admin\PedreiroBaseController as BaseController;
-use Support\Components\Database\Schema\SchemaManager;
-use Support\Events\BreadDataAdded;
-use Support\Events\BreadDataDeleted;
-use Support\Events\BreadDataRestored;
-use Support\Events\BreadDataUpdated;
-use Support\Events\BreadImagesDeleted;
 
 /**
  * The base controller is gives Decoy most of the magic/for-free mojo
